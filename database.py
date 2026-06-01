22aca6466068611710ce3d75e09b0d41100925bb
geetha <puligaddageeetavani@gmail.com>
2026-06-01 12:29:01 +0530

    Use DATABASE_URL environment variable

--- database.py
+++ database.py
@@ -1,7 +1,10 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-DATABASE_URL="postgresql://postgres:Geetha%40123@localhost/Database"
+import os
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine=create_engine(DATABASE_URL)
 SessionLocal=sessionmaker(bind=engine,autocommit=False,autoflush=False)
 Base=declarative_base()
--- schemas.py
+++ schemas.py
@@ -15,15 +15,17 @@ class job(BaseModel):
 class showuser(BaseModel):
     name:str
     email:str
+    class config:
+        orm_mode=True
 class updatejob(BaseModel):
     job_status:str
 class showjob(BaseModel):
     company_name:str
     job_role:str
-    job_url:Optional[str]
     job_status:str
-    applied_date:Optional[date]
     user_id:int
+    class config:
+        orm_mode=True
 class Tokendata(BaseModel):
     email:str
     
